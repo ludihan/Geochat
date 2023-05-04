7a324f60cc563d78edb170ab788a48e48bf2bc62
tiagof31 <tiagofarias31@hotmail.com>
2023-05-04 18:09:22 -0300

    Modificação Tamanho Label

--- app/src/main/java/com/example/geochat/MainActivity.kt
+++ app/src/main/java/com/example/geochat/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,11 +29,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -79,21 +86,30 @@ fun CredentialsTextField(
 
     var text by remember { mutableStateOf(text) }
 
-    TextField(
-        value = "",
+    TextField(value = "AAA",
+        modifier = Modifier.size(240.dp, 30.dp),
+        shape = RectangleShape,
         onValueChange = { text = it },
-        label = { Text(text) },
+        label = {
+            Column(verticalArrangement = Arrangement.Top) {
+                Text(
+                    text = text,
+                    fontSize = 12.sp,
+                    fontStyle = FontStyle.Italic
+                )
+            }
+        },
         colors = TextFieldDefaults.textFieldColors(
             containerColor = Gray
         )
     )
 
 }
+
 @Composable
 private fun App() {
     Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+        modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
     ) {
         Column(
             verticalArrangement = Arrangement.SpaceEvenly,
@@ -102,8 +118,7 @@ private fun App() {
 
             //Coluna Geochat e Seta
             Column(
-                verticalArrangement = Arrangement.SpaceEvenly,
-                horizontalAlignment = Alignment.Start
+                verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.Start
             ) {
                 Text(
                     text = "Geochat",
@@ -114,7 +129,10 @@ private fun App() {
             }
 
             //Coluna Usuário e Senha
-            Column {
+            Column(
+                //verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier.size(240.dp, 70.dp)
+            ) {
                 CredentialsTextField(text = "Nome") {
                     /*TODO*/
                 }
@@ -126,8 +144,7 @@ private fun App() {
             LoginButton(text = "Login", buttonColors = GrayButtonColors()) {
                 Log.d(TAG, "App: Clicou em Login!")
             }
-            Text(
-                text = "Esqueci a senha",
+            Text(text = "Esqueci a senha",
                 color = Color.Blue,
                 style = TextStyle(textDecoration = TextDecoration.Underline),
                 modifier = Modifier.clickable {})
