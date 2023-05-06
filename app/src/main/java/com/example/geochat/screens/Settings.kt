1da99c95b8a361fbd1af4d5d19d5b2c82a7d5bbd
ludihan <luccad.han@gmail.com>
2023-05-06 16:05:40 -0300

    removed local.properties from git

---
+++ app/src/main/java/com/example/geochat/screens/Settings.kt
@@ -0,0 +1,13 @@
+package com.example.geochat.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+/*
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+    )
+}*/
